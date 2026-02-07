703376ef98b3f4cc43d284096cd06de17261c334
0xall3x <76539258+0xall3x@users.noreply.github.com>
2026-02-07 14:45:11 +0100

    v0.1.1

--- setup.sh
+++ setup.sh
@@ -7,7 +7,4 @@ echo "[*] Actualizando pip e instalando dependencias..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# El comando 'toutatis' a veces no se registra en el PATH global, 
-# pero estará disponible en ./venv/bin/toutatis
 echo "[+] Instalación completada."
-echo "[!] IMPORTANTE: Antes de ejecutar, usa: source venv/bin/activate"
